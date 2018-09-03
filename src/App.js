ace6b97bef4789d622a881559145a0cb7a19815f
vetalxpro <vetalpro.exe@gmail.com>
2018-09-03 09:48:57 +0300

    feature

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello from AWS!
+          Hello from AWS and Docker!
         </p>
       </div>
     );
